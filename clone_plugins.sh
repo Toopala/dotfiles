c5116a6f23c73567edf9568eb09f226770527940
Toopala <miika.saastamoinen@gmail.com>
2016-12-16 10:11:42 +0200

    adds plugin clone script

---
+++ clone_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/.vim/bundle;
+git clone https://github.com/scrooloose/nerdtree;
+git clone https://github.com/rking/ag.vim;
+git clone https://github.com/kien/ctrlp.vim;
+git clone https://github.com/mfukar/robotframework-vim;
+git clone https://github.com/scrooloose/syntastic;
+git clone https://github.com/leshill/vim-json;
+git clone https://github.com/powerline/powerline;
+git clone https://github.com/fab-du/vim-sensible;
